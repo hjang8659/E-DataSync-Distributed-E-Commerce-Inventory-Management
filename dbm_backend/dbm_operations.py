57c5aba98497a105bf98ff801ef601ab167802c9
Shahzaib Saqib <warraich@usc.edu>
2024-02-24 17:01:48 -0800

    added comments

--- dbm_backend/dbm_operations.py
+++ dbm_backend/dbm_operations.py
@@ -1,21 +1,21 @@
 from sqlalchemy import create_engine
 from hashing import hash_prod
 
-class dbm_operations():
-    def __init__(self, table, text):
-        self.text = text
-        self.table = table
+class dbm_operations(): # before calling the methods of this class: 1. Get table name 2. Get CLI input 3. Call relevant functions based on contains method
+    def __init__(self):
+        self.server_string = 'mysql+pymysql://myuser3:Dsc!5602023@104.32.175.9:3306/'
 
-    def insert(self,):
+    def insert(self, table, text): # insert record in hashed db
+
         pass
 
-    def modify(self,):
+    def modify(self, table, text): # modify records in both dbs
         pass
 
-    def delete(self,):
+    def delete(self, table, text): # perform on both dbs, return successful/unsuccessful message, explaining reason
         pass
 
-    def select(self,):
+    def select(self, table, text): # perform on both dbs, return combine results if successful else return unsuccessful message
         pass
     
     
