bf90085dce7ce5f9e667d15a6198e4b94368a0c9
Daniel <79027463+hjang8659@users.noreply.github.com>
2024-02-27 05:09:19 +0000

    updated sql query

--- dbm_frontend/pages/2_Developer.py
+++ dbm_frontend/pages/2_Developer.py
@@ -3,6 +3,7 @@ import subprocess
 import pandas as pd
 from sqlalchemy import create_engine, text
 from sqlalchemy.exc import SQLAlchemyError
+import sqlparse
 
 def authenticate():
     password = st.text_input("Enter Password:", type="password")
@@ -19,7 +20,7 @@ def run_dev_page():
     authenticated = authenticate()
 
     if authenticated:
-        st.write("Authentication successful!")
+        st.success("Authentication successful!")
         st.write("This is the developer page where you can perform development tasks.")
 
         # Add developer-specific content and functionality here
@@ -28,28 +29,29 @@ def run_dev_page():
         st.write("## Developer Command Line Interface")
 
         # Allow the user to input a command
-        cli_command = st.text_input("Enter a command:")
+        cli_command = st.text_area("Enter a command then click Run Command:")
 
         # Execute the command and display the output
-        if cli_command:
+        if st.button("Run Command"):
             st.write("### Command Output:")
             try:
-                if cli_command.startswith("SHOW TABLES;"):
-                    # Connect to the database using SQLAlchemy
+                if sqlparse.parse(cli_command):
                     engine = create_engine('mysql+pymysql://myuser3:Dsc!5602023@104.32.175.9:3306/mydatabase2')
-                    
+
                     # Use parameterized query for safety
                     query = text(cli_command)
                     records = pd.read_sql(query, engine)
                     st.write(records)
                 else:
-                    # For other commands, use subprocess
+                    # For other commands, use subprocess cautiously
                     output = subprocess.check_output(cli_command, shell=True, text=True)
                     st.code(output, language='text')
-            except (subprocess.CalledProcessError, SQLAlchemyError) as e:
+            except subprocess.CalledProcessError as e:
                 st.error(f"Error executing command: {e}")
-    else:
-        st.error("Authentication failed. Please enter the correct password.")
+            except SQLAlchemyError as e:
+                st.error(f"SQLAlchemy Error: {e}")
+            except Exception as e:
+                st.error(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     run_dev_page()
