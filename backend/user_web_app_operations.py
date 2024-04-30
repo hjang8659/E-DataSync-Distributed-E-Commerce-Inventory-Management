72e1328a2c1e1a33648f0294e6ec0ace88039f91
Shahzaib Saqib <warraich@usc.edu>
2024-04-29 20:07:37 -0700

    commented redundant code blocks

--- backend/user_web_app_operations.py
+++ backend/user_web_app_operations.py
@@ -1,9 +1,6 @@
 from backend.hashing import hash_supplier
 from backend.dbm_ui_operations import DBMOperations
 
-#TODO: make general insert function which can insert into any of the 4 tables correctly
-# - make 2nd hash function even/odd
-
 class UserOperations:
     """
     Class to perform User operations.
@@ -20,23 +17,6 @@ class UserOperations:
         """
         function to insert table_name with columns and values.
         """
-        # Insert from opr is only for supplier name, would need to change it to work with any table
-        # INSERT INTO table_name (column1, column2, column3, ...)
-        # VALUES (value1, value2, value3, ...);
-
-        # SELECT COLUMN_NAME, DATA_TYPE 
-        # FROM INFORMATION_SCHEMA.COLUMNS 
-        # WHERE TABLE_NAME = 'tbl_name';
-        #query = f"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
-        #flag, res = self.opr.select(query)
-        #data_type_dict = {key: value for key, value in res}
-        #print(data_type_dict)
-        #for i in range(len(columns)):
-        #    if data_type_dict[columns[i]] == "date" or data_type_dict[columns[i]] == "text" or data_type_dict[columns[i]] == "varchar":
-        #        values[i] = "\'" + values[i] + "\'"
-
-
-
         if len(columns) != len(values):
             print("Error: Length of columns and values do not match.")
 
@@ -52,22 +32,6 @@ class UserOperations:
         """
         function to modify table_name with set attributes, operators, and values. Lastly, use the conditions. 
         """
-        # UPDATE table_name
-        # SET column1 = value1, column2 = value2, ...
-        # WHERE condition;
-
-        #query = f"SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '{table_name}'"
-        #flag, res = self.opr.select(query)
-        #data_type_dict = {key: value for key, value in res}
-        #print(data_type_dict)
-        #for i in range(len(columns)):
-        #    if data_type_dict[columns[i]] == "date" or data_type_dict[columns[i]] == "text" or data_type_dict[columns[i]] == "varchar":
-        #        vals[i] = "\'" + vals[i] + "\'"
-
-        #for i in range(len(key)):
-        #    if data_type_dict[key[i]] == "date" or data_type_dict[key[i]] == "text" or data_type_dict[key[i]] == "varchar":
-        #        search[i] = "\'" + search[i] + "\'"
-
 
         if len(key) != len(search):
             print("Error: Length of key and search do not match.")
@@ -98,15 +62,15 @@ class UserOperations:
         print(flag, res)
         return flag, res
     
-    def searchMany(self, table_name, attributes, col, search_cond):
-        """
-        function to search table_name with set attributes. 
-        """
+    # def searchMany(self, table_name, attributes, col, search_cond):
+    #     """
+    #     function to search table_name with set attributes. 
+    #     """
 
-        str_attributes = ", ".join(attributes)
-        flag, res = self.opr.select(f'SELECT {str_attributes} FROM {table_name}')
-        print(flag, res)
-        return flag, res
+    #     str_attributes = ", ".join(attributes)
+    #     flag, res = self.opr.select(f'SELECT {str_attributes} FROM {table_name}')
+    #     print(flag, res)
+    #     return flag, res
 
     
     def delete(self, table_name, conditions):
@@ -119,22 +83,22 @@ class UserOperations:
         print(flag, res)
         return flag, res
     
-    def deleteOne(self, table_name, conditions):
-        """
-        function to delete 1 row from table_name with conditions. 
-        """
-        # Delete based on specific primary key, finish
-        str_conditions = ", ".join(conditions)
-        flag, res = self.opr.select(f'DELETE FROM {table_name} WHERE {str_conditions}')
-        print(flag, res)
-        return flag, res
+    # def deleteOne(self, table_name, conditions):
+    #     """
+    #     function to delete 1 row from table_name with conditions. 
+    #     """
+    #     # Delete based on specific primary key, finish
+    #     str_conditions = ", ".join(conditions)
+    #     flag, res = self.opr.select(f'DELETE FROM {table_name} WHERE {str_conditions}')
+    #     print(flag, res)
+    #     return flag, res
     
-    def deleteMany(self, table_name, conditions):
-        """
-        function to delete many rows from table_name with conditions. 
-        """
+    # def deleteMany(self, table_name, conditions):
+    #     """
+    #     function to delete many rows from table_name with conditions. 
+    #     """
         
-        str_conditions = ", ".join(conditions)
-        flag, res = self.opr.select(f'DELETE FROM {table_name} WHERE {str_conditions}')
-        print(flag, res)
-        return flag, res
+    #     str_conditions = ", ".join(conditions)
+    #     flag, res = self.opr.select(f'DELETE FROM {table_name} WHERE {str_conditions}')
+    #     print(flag, res)
+    #     return flag, res
